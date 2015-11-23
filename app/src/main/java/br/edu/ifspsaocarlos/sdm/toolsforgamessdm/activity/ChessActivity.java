dfc2bbb60dd7cc87d99fe46c1332b4f4f8b255e0
Andre Luis <andre.gomes@s2it.com.br>
2015-11-23 21:56:29 -0200

    Finalizado timer invertido

--- app/src/main/java/br/edu/ifspsaocarlos/sdm/toolsforgamessdm/activity/ChessActivity.java
+++ app/src/main/java/br/edu/ifspsaocarlos/sdm/toolsforgamessdm/activity/ChessActivity.java
@@ -10,15 +10,24 @@ import android.widget.Toast;
 
 import br.edu.ifspsaocarlos.sdm.toolsforgamessdm.R;
 
+import static android.widget.Toast.LENGTH_LONG;
+
 public class ChessActivity extends AppCompatActivity {
 
-    public static final String DEFAULT_DURATION_INFO = "Tempo padrão de 2 min.";
+    public static final String DEFAULT_DURATION_INFO = "Tempo padrão de 60 min.";
+    public static final String TIME_FINISH = "Acabou o tempo :(";
+    public static final int DEFAULT_DURATION = 60;
+    public static final String WHITE_PLAYER_SHOULD_START = "Jogador Branco deve começar.";
 
     private Button buttonPlayer1;
     private Button buttonPlayer2;
-    private ChessTimer timer;
+    private ChessTimer white;
+    private ChessTimer black;
     private int playerTurn = -1;
     private int chessTime;
+    private Button button;
+    private boolean firstTurnWhite = false;
+    private boolean firstTurnBlack = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,87 +37,115 @@ public class ChessActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         //recupera os componentes
-        buttonPlayer1 = (Button)findViewById(R.id.buttonPlayer1);
-        buttonPlayer2 = (Button)findViewById(R.id.buttonPlayer2);
+        buttonPlayer1 = (Button) findViewById(R.id.buttonPlayer1);
+        buttonPlayer2 = (Button) findViewById(R.id.buttonPlayer2);
 
         //Verifica se existe tempo selecionado, senão utiliza o tempo padrão
-        if (getIntent().hasExtra("chessTime")){
-            chessTime = (int) getIntent().getSerializableExtra("chessTime");
-            timer = new ChessTimer(chessTime*1000);
-        }else {
+        if (getIntent().hasExtra("chessTime")) {
+            chessTime = (int) getIntent().getSerializableExtra("chessTime") * 60000;
+        } else {
             Toast.makeText(this, DEFAULT_DURATION_INFO, Toast.LENGTH_SHORT).show();
-            timer = new ChessTimer();
+            chessTime = DEFAULT_DURATION * 60000;
         }
+        buttonPlayer1.setText(formatTime(chessTime));
+        buttonPlayer2.setText(formatTime(chessTime));
+        white = new ChessTimer(chessTime);
+        black =new ChessTimer(chessTime);
+    }
+
+    //Formata o tempo para string
+    private String formatTime(int chessTime) {
+        String min = String.format("%02d", chessTime / 60000);
+        int seg = (chessTime % 60000) / 1000;
+        return String.format("%s:%02d", min, seg);
     }
 
     //Jogador clica para parar o tempo
-    public void onPlayerClick(View view){
-        if(playerTurn == -1){
-            //clearButtons();
+    public void onPlayerClick(View view) {
+        if (playerTurn == -1) {
             playerTurn = view.equals(buttonPlayer1) ? 1 : 2;
-            timer.start((Button)view);
-        }
-        else{
-
+            //Alerta informando que quem deve começar é o jogador branco
+            if (playerTurn != 2) {
+                playerTurn = -1;
+                Toast.makeText(this, WHITE_PLAYER_SHOULD_START, Toast.LENGTH_SHORT).show();
+            } else {
+                white.start((Button) view);
+                firstTurnWhite = true;
+            }
+        } else {
             //jogador cuja não é a vez clicou no botão
-            if( (view.equals(buttonPlayer2) && playerTurn == 1) || (view.equals(buttonPlayer1) && playerTurn == 2)){
+            if ((view.equals(buttonPlayer2) && playerTurn == 1) || (view.equals(buttonPlayer1) && playerTurn == 2)) {
                 return;
             }
+            change();
 
-           change();
+            //Marca a primeira vez que o botao foi abe
+            if (firstTurnWhite && !firstTurnBlack) {
+                firstTurnBlack = true;
+            }
         }
     }
 
     /**
-     *
-     * Limpa o texto dos botões
-     */
-    private void clearButtons(){
-        buttonPlayer1.setText("");
-        buttonPlayer2.setText("");
-    }
-
-    /**
-     *
      * Inverte os jogadores
-     *
      */
-    private void change(){
-        playerTurn = playerTurn == 1 ? 2 : 1;
-        timer.cancel();
-        //clearButtons();
+    private void change() {
+        playerTurn = playerTurn == 2 ? 1 : 2;
+        String[] split;
+        int milliUntilFinished;
 
-        timer.start(playerTurn == 1 ? buttonPlayer1 : buttonPlayer2);
+        //Verifica qual o jogador para recuperar o tempo restante do botão
+        if (playerTurn == 1) {
+            split = buttonPlayer1.getText().toString().split(":");
+        } else {
+            split = buttonPlayer2.getText().toString().split(":");
+        }
+
+        //se já foi a primeira vez que o botão foi apertado. calcula o tempo restante
+        if (firstTurnWhite && firstTurnBlack) {
+            milliUntilFinished = Integer.valueOf(split[0]) * 60000 + Integer.valueOf(split[1]) * 1000;
+        } else {//se não, usa o tempo default
+            milliUntilFinished = chessTime;
+        }
+
+        //Se for o jogador preto quem clicou
+        if (playerTurn == 1) {
+            white.cancel();
+            black = new ChessTimer(milliUntilFinished);
+            black.start(playerTurn == 1 ? buttonPlayer1 : buttonPlayer2);
+        } else {
+            black.cancel();
+            white = new ChessTimer(milliUntilFinished);
+            white.start(playerTurn == 1 ? buttonPlayer1 : buttonPlayer2);
+        }
     }
 
-    private class ChessTimer extends CountDownTimer{
-        private final static int DEFAULT_DURATION = 120000; //2min
-        private Button buttonPrint;
+    //Mostra a mensagem de tempo acabado
+    private void showMessage() {
+        Toast.makeText(this, TIME_FINISH, LENGTH_LONG).show();
+    }
 
-        public ChessTimer() {
-            super(DEFAULT_DURATION, 1000);
-        }
+    private class ChessTimer extends CountDownTimer {
+        private Button buttonPrint;
 
         public ChessTimer(final int chessTime) {
             super(chessTime, 1000);
         }
 
-        public void start(Button buttonPrint){
+        public void start(Button buttonPrint) {
             this.buttonPrint = buttonPrint;
             super.start();
         }
 
         public void onTick(long millisUntilFinished) {
-            String min = String.format("%02d", millisUntilFinished/60000);
-            int seg = (int)( (millisUntilFinished%60000)/1000);
-
-            buttonPrint.setText(String.format("%s:%02d", min, seg));
+            buttonPrint.setText(formatTime((int) millisUntilFinished));
         }
 
         @Override
         public void onFinish() {
-            change();
+            showMessage();
+
         }
     }
-}
 
+}
